402b24244eb4973cd55f9cf423b5ab299215369a
oyasuryana <oyasuryana@gmail.com>
2020-12-19 20:45:29 +0700

    Menambahkan bootstrap di folder public, merubah file index.js, merubah file file views/index.hbs

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const path =require('path');
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
 
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','hbs');
+app.use(express.static('public'));
+
 // route home
 app.get('/',(req,res)=>{
-    
+    res.render('index');    
+});
 
+app.listen(8000,()=>{
+    console.log('Server bejalan di port 8000');
 })
