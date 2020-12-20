8ee81d33f5d62aace30048db8833b5069c87c754
oyasuryana <oyasuryana@gmail.com>
2020-12-20 19:03:14 +0700

    Update index.js, update view BukuTamu.hbs dan index.hbs

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const path =require('path');
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser=require('body-parser');
 const app = express();
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','hbs');
+
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static('public'));
 
 // route home
@@ -15,11 +18,18 @@ app.get('/',(req,res)=>{
 // route about
 app.get('/about',(req,res)=>{
     res.render('about')
-})
+});
 
 // route form buku tamu
-app.get('/bukutamu',()=>{
+app.get('/bukutamu',(req,res)=>{
     res.render('BukuTamu');
+});
+
+app.post('/bukutamu',(req,res)=>{
+    res.render('BukuTamu',{
+        nama    : req.body.txtNama,
+        alamat  : req.body.txtAlamat   
+    });
 });
 
 app.listen(8000,()=>{
